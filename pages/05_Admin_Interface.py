2cf5b0239e6491f466879ed51b3498ad991b9857
treyhulse <treyhulse3@gmail.com>
2024-08-31 13:10:49 -0500

    Update 05_Admin_Interface.py

--- pages/05_Admin_Interface.py
+++ pages/05_Admin_Interface.py
@@ -1,12 +1,14 @@
+# 05_Admin_Interface.py
+
 import streamlit as st
 from utils.mongo_connection import get_mongo_connection
-from utils.auth import authenticate_user, check_permission
+from utils.auth import authenticate_user
 import datetime
 
 # Get the MongoDB connection
 db = get_mongo_connection()
 
-st.title("🏈 Admin - Make a Bet")
+st.title("🏈 Make a Bet")
 
 # Get user input for authentication
 username = st.text_input("Username")
@@ -17,31 +19,27 @@ user = authenticate_user(username, password, db)
 if user:
     st.write(f"Welcome {user['username']}!")
     
-    # Check if the user has permission to post bets
-    if check_permission(user, "post_bets", db):
-        # Bet details
-        team_1 = st.text_input("Team 1", value="")
-        team_2 = st.text_input("Team 2", value="")
-        bet_type = st.selectbox("Bet Type", options=["Moneyline", "Spread", "Total", "Prop Bet"])
-        odds = st.text_input("Odds", value="")
-        amount = st.number_input("Amount", min_value=0.0)
-        date = st.date_input("Date", value=datetime.date.today())
+    # Allow any authenticated user to post bets
+    team_1 = st.text_input("Team 1", value="")
+    team_2 = st.text_input("Team 2", value="")
+    bet_type = st.selectbox("Bet Type", options=["Moneyline", "Spread", "Total", "Prop Bet"])
+    odds = st.text_input("Odds", value="")
+    amount = st.number_input("Amount", min_value=0.0)
+    date = st.date_input("Date", value=datetime.date.today())
 
-        if st.button("Post Bet"):
-            # Insert the new bet into MongoDB
-            bet = {
-                "team_1": team_1,
-                "team_2": team_2,
-                "bet_type": bet_type,
-                "odds": odds,
-                "amount": amount,
-                "date": str(date),
-                "status": "posted",
-                "created_by": user['username']
-            }
-            db['bets'].insert_one(bet)
-            st.success("Bet posted successfully!")
-    else:
-        st.error("You do not have permission to post bets.")
+    if st.button("Post Bet"):
+        # Insert the new bet into MongoDB
+        bet = {
+            "team_1": team_1,
+            "team_2": team_2",
+            "bet_type": bet_type,
+            "odds": odds,
+            "amount": amount,
+            "date": str(date),
+            "status": "posted",
+            "created_by": user['username']
+        }
+        db['bets'].insert_one(bet)
+        st.success("Bet posted successfully!")
 else:
     st.error("Invalid username or password.")
